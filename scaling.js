a7a67a1bccde225ee048b236f4ffa90fc2398315
ProfSeaquill <mosesu@gmail.com>
2025-09-06 03:12:11 -0700

    Update scaling.js

--- scaling.js
+++ scaling.js
@@ -1,1 +1,9 @@
-
+export const cost = {
+  dmg: (L)=> Math.floor(10 * Math.pow(1.25, L.dmg)),
+  rate:(L)=> Math.floor(15 * Math.pow(1.3,  L.rate)),
+  burn:(L)=> Math.floor(40 * Math.pow(1.35, L.burn)),
+  pierce:(L)=> Math.floor(60 * Math.pow(1.4,  L.pierce)),
+};
+export const waveHP = (n)=> Math.floor(60 * Math.pow(1.18, n-1));
+export const bossHP = (n)=> Math.floor(waveHP(n) * 10);
+export const reward = (n, boss)=> boss ? Math.floor(50 + n*12) : Math.floor(10 + n*3);
