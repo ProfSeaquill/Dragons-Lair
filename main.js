96ae922328149f606743b7bb81b9c41dc263217e
ProfSeaquill <mosesu@gmail.com>
2025-09-06 03:11:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+import { load } from './state.js';
+import { createRenderer } from './render.js';
+import { startWave } from './combat.js';
+import { tryUpgradeDamage, tryUpgradeRate, tryUpgradeBurn, tryUpgradePierce } from './upgrades.js';
+import { wireUI, updateUI } from './ui.js';
 
+const canvas = document.getElementById('game');
+createRenderer(canvas);
+
+wireUI({
+  onStart: startWave,
+  onUpgrade: {
+    dmg:   tryUpgradeDamage,
+    rate:  tryUpgradeRate,
+    burn:  tryUpgradeBurn,
+    pierce:tryUpgradePierce
+  }
+});
+
+load();
+updateUI();
+setTimeout(()=> startWave(), 600);
