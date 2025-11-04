f16921a77447b3ae4f46a0d0fc3debaed81abdb6
ProfSeaquill <mosesu@gmail.com>
2025-11-03 19:57:40 -0800

    Delete ai directory

--- ai/metrics.js
+++
@@ -1,8 +0,0 @@
-// ai/metrics.js
-
-export function updateEnemyDistance(e, gs) {
-  if (!gs.exit) return;
-  const dx = Math.abs(e.tileX - gs.exit.tileX);
-  const dy = Math.abs(e.tileY - gs.exit.tileY);
-  e.distToExit = dx + dy;
-}
