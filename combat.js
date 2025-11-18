4a333786b48ac6490c2d1e28938c5eaecab1d1f0
ProfSeaquill <mosesu@gmail.com>
2025-11-18 13:58:20 -0800

    Update combat.js

--- combat.js
+++ combat.js
@@ -1509,8 +1509,10 @@ function _cadenceSpecials(wave, cadence, FLAGS) {
   // If truthy, we spawn a kingsguard exactly *one wave before* each boss wave
   const kgBeforeBoss = cadence?.kingsguardEvery ?? FLAGS.kingsguardEvery;
 
-  // Story script: any wave in BOSS_SCHEDULE is a boss wave (Arthur, named knights, etc.)
-  const scriptedBoss = !!(BOSS_SCHEDULE && BOSS_SCHEDULE[wave]);
+    // Story script: any wave in BOSS_SCHEDULE is a boss wave (Arthur, named knights, etc.)
+  const bossId       = BOSS_SCHEDULE?.[wave] || null;
+  const scriptedBoss = !!bossId;
+
 
   // Is the *next* wave a boss (either by cadence or by story)?
   const nextWaveIsBoss =
@@ -1528,6 +1530,17 @@ function _cadenceSpecials(wave, cadence, FLAGS) {
   if (kgBeforeBoss && nextWaveIsBoss) {
     out.push('kingsguard');
   }
+
+    // Arthur special-case: pack this wave with extra kingsguard
+  if (bossId === 'king arthur' || bossId === 'arthur') {
+    const EXTRA_KG = 3; // tweak up/down as desired
+    for (let i = 0; i < EXTRA_KG; i++) {
+      out.push('kingsguard');
+    }
+  }
+
+  return out;
+}
 
   return out;
 }
