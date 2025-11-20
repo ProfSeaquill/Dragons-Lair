6e243abf240f1f263eb3a99fbe4239bdfecf78df
ProfSeaquill <mosesu@gmail.com>
2025-11-20 14:31:22 -0800

    Update state.js

--- state.js
+++ state.js
@@ -576,6 +576,12 @@ export const GameState = {
   reqRoar:     false,
   reqStomp:    false,
 
+  // Vents
+gs.flameVents = gs.flameVents || []; // [{ x, y }]
+gs.maxFlameVents = 3;                // or from tuning.json later
+gs.flameVentsAvailable = 3;          // pool the player can place
+
+
   // Per-cell edge walls: Map<"x,y", {N,E,S,W:boolean}>
   cellWalls: new Map(),
 
